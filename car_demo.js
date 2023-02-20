87b4f155dfb41d70988babc3b323683c24a7588f
Dnuangkamon <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-02-21 02:11:27 +0700

    เพิ่มรถ js ทำ card เสร็จแล้ว

--- car_demo.js
+++ car_demo.js
@@ -2,13 +2,14 @@ var app = new Vue({
     el: '#app',
     data: {
         cars: [{
-
                 brand: 'Toyota',
                 model: 'Yaris',
                 img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/yaris_2016-2019.png',
                 seat: 4,
                 bag: 1,
-                price: 1190
+                price: 1190,
+                other: false,
+                show_toyota: true
             },
             {
                 brand: 'Toyota',
@@ -16,14 +17,17 @@ var app = new Vue({
                 img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/Fortuner_2016-2018_VbSeQ0L.png',
                 seat: 7,
                 bag: 3,
-                price: 3500
+                price: 3500,
+                other: false
             }, {
                 brand: 'Toyota',
                 model: 'Camry',
                 img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/Car_with_Driver_210304_29.jpg',
                 seat: 4,
                 bag: 2,
-                price: 3500
+                price: 3500,
+                other: false,
+                show_toyota: true
             },
             {
                 brand: 'Toyota',
@@ -31,7 +35,9 @@ var app = new Vue({
                 img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/Toyota_Alphard_2018_yJyb1bM.png',
                 seat: 7,
                 bag: 5,
-                price: 9900
+                price: 9900,
+                other: false,
+                show_toyota: true
             },
             {
                 brand: 'Toyota',
@@ -39,7 +45,8 @@ var app = new Vue({
                 img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/Toyota_Commuter__z4uZ6f8.png',
                 seat: 9,
                 bag: 5,
-                price: 3000
+                price: 3000,
+                other: false
             },
             {
                 brand: 'Nissan',
@@ -47,7 +54,8 @@ var app = new Vue({
                 img: 'https://cdn2.rcstatic.com/images/car_images/web/nissan/almera_lrg.jpg',
                 seat: 5,
                 bag: 1,
-                price: 1141
+                price: 1141,
+                other: false
             },
             {
                 brand: 'Nissan',
@@ -55,7 +63,8 @@ var app = new Vue({
                 img: 'https://cdn2.rcstatic.com/images/car_images/web/nissan/micra_lrg.jpg',
                 seat: 4,
                 bag: 1,
-                price: 975
+                price: 975,
+                other: false
             },
             {
                 brand: 'Nissan',
@@ -63,7 +72,9 @@ var app = new Vue({
                 img: 'https://www.car250.com/wp-content/uploads/2019/07/Nissan-Kicks-2019-23.png',
                 seat: 5,
                 bag: 1,
-                price: 2190
+                price: 2190,
+                other: false,
+                show_nissan: true
             },
             {
                 brand: 'Nissan',
@@ -71,23 +82,28 @@ var app = new Vue({
                 img: 'https://blog.hamrobazaar.com/wp-content/uploads/2020/12/Nissan-Leaf.png',
                 seat: 4,
                 bag: 1,
-                price: 1990
+                price: 8900,
+                other: false,
+                show_nissan: true
             },
             {
                 brand: 'Nissan',
                 model: 'Teana',
                 img: 'https://fj.lnwfile.com/_/fj/_raw/hb/al/m9.png',
-                seat: 5,
+                seat: 4,
                 bag: 1,
-                price: 1150
+                price: 7150,
+                other: false,
+                show_nissan: true
             },
             {
                 brand: 'Nissan',
                 model: 'Urvan',
                 img: 'https://media.discordapp.net/attachments/1072181252964233328/1077292220387958934/77-772232_01-nv350-urvan-exterior-hd-png-download-removebg-preview.png',
-                seat: 4,
-                bag: 1,
-                price: 19900
+                seat: 9,
+                bag: 5,
+                price: 9900,
+                other: false
             },
             {
                 brand: 'Honda',
@@ -95,7 +111,8 @@ var app = new Vue({
                 img: 'https://cdn2.rcstatic.com/images/car_images/web/honda/city_lrg.jpg',
                 seat: 4,
                 bag: 1,
-                price: 1259
+                price: 1259,
+                other: false
             },
             {
                 brand: 'Honda',
@@ -103,7 +120,8 @@ var app = new Vue({
                 img: 'https://cdn2.rcstatic.com/images/car_images/web/honda/brv_lrg.jpg',
                 seat: 5,
                 bag: 3,
-                price: 2805
+                price: 2805,
+                other: false
             },
             {
                 brand: 'Honda',
@@ -111,15 +129,17 @@ var app = new Vue({
                 img: 'https://cdn2.rcstatic.com/images/car_images/new_images/honda/civic_sedan_lrg.jpg',
                 seat: 5,
                 bag: 2,
-                price: 1579
+                price: 1579,
+                other: false
             },
             {
-                brand: 'Mitsubishi',
-                model: 'Pajero Sport',
-                img: 'https://cdn2.rcstatic.com/images/car_images/web/mitsubishi/pajero_4_doors_lrg.jpg',
-                seat: 7,
+                brand: 'Mercedes Benz Sport',
+                model: null,
+                img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/02.png',
+                seat: 2,
                 bag: 2,
-                price: 2553
+                price: 14900,
+                other: true
             },
             {
                 brand: 'Hyundai',
@@ -127,7 +147,25 @@ var app = new Vue({
                 img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/Car_with_Driver_210304_71.jpg',
                 seat: 7,
                 bag: 3,
-                price: 3300
+                price: 3300,
+                other: true
+            }, {
+                brand: 'MG',
+                model: 'HS',
+                img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/MG_HS_2021.png',
+                seat: 7,
+                bag: 2,
+                price: 2553,
+                other: true
+            },
+            {
+                brand: 'BMW Series 7',
+                model: null,
+                img: 'https://car-with-driver.s3-ap-southeast-1.amazonaws.com/cars/bmw_series_7.png',
+                seat: 4,
+                bag: 2,
+                price: 15000,
+                other: true
             },
         ],
         isBrand: false,
