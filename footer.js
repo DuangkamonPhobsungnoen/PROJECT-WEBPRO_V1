34290c496c972968a790de0d91e47cc38731d72a
Dnuangkamon <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-03-02 00:41:40 +0700

    แก้ footer ทุกหน้า ลบไฟล์ที่ไม่ใช้แล้วออก

---
+++ footer.js
@@ -0,0 +1,51 @@
+  Vue.component('footer-div', {
+      name: 'footerF',
+      template: `
+    <footer class="is-color-footer">
+    <div class="column is-size-4 has-text-white">
+        <p class="column_number">
+            <img src="https://media.discordapp.net/attachments/1072181252964233328/1080536072989192294/image_40.png" height="25" width="25">
+            <span class="pl-2">ChaoCarHUB</span>
+        </p>
+    </div>
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="column"></div>
+    <div class="columns pl-4 is-size-6 has-text-white ">
+        <div class="column">
+            <p class="column_number">
+                <img src="https://media.discordapp.net/attachments/1072181252964233328/1080535831049146408/image_66.png">
+                <span class="pl-2">สอบถามข้อมูลเพิ่มเติม
+                    <br>สามารถติดต่อ 065-5674-8764</span>
+            </p>
+        </div>
+        <div class="column">
+            <p class=" column_number">
+                <img src="https://media.discordapp.net/attachments/1072181252964233328/1080535830835241081/image_63.png">
+                <span class="pl-2">
+                    สอบถามข้อมูลเพิ่มเติม
+                    <br>ผ่านทางไลน์ OA @Chaocarhub</span>
+            </p>
+        </div>
+        <div class="column">
+            <p class=" column_number">
+                <img src="https://media.discordapp.net/attachments/1072181252964233328/1080535830608744598/image_64.png">
+                <span class="pl-2">
+                    สอบถามข้อมูลเพิ่มเติม
+                    <br>ผ่านทางเฟชบุ๊ค Chaocarhub</span>
+            </p>
+        </div>
+        <div class="column">
+            <p class=" column_number">
+                <img src="https://media.discordapp.net/attachments/1072181252964233328/1080535830352896190/image_65.png">
+                <span class="pl-2">
+                    สอบถามข้อมูลเพิ่มเติมทางอีเมล <br>
+                    Chaocarhub@gmail.com</span>
+            </p>
+        </div>
+</footer>`
+  })
+  new Vue({
+      el: "#footer",
+      data: {},
+  })
